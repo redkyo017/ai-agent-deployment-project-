6833eb95b912ed55f8ca402e8ae19f4f0cd427e2
Duc Hung <hunghd.uit@gmail.com>
2025-10-26 21:39:39 +0700

    day 5 - config docker

--- saas/next.config.ts
+++ saas/next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
+  // /* config options here */
+  // reactStrictMode: true,
+  output: 'export',  // This exports static HTML/JS files
+  images: {
+    unoptimized: true  // Required for static export
+  }
 };
 
 export default nextConfig;
